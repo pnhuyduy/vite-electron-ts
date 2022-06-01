68441875df1a5ce121478ed5d0dbc361d9cccdee
pnhuyduy <huyduy.work@gmail.com>
2022-06-01 09:27:23 +0700

    update db check

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -1,22 +1,18 @@
 import path from 'path'
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, BrowserWindow, dialog } from 'electron'
 import sqlite3 from 'sqlite3'
 
 const dbpath = path.join(app.getPath('userData'), 'vite-electron-ts-sqlite3.db')
-const db = new sqlite3.Database(dbpath)
 
-ipcMain.handle('check-database', async event => {
-  db.serialize(() => {
-    db.run('ELECT rowid AS id, info FROM lorem')
-
-    const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-    for (let i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + i)
+ipcMain.handle('check-database', async () => {
+  const db = new sqlite3.Database(dbpath, err => {
+    if (err) {
+      console.log(err)
+    } else {
+      const mainWindow = BrowserWindow.getAllWindows()[0]
+      dialog.showMessageBoxSync(mainWindow, {
+        message: 'DB connected',
+      })
     }
-    stmt.finalize()
-
-    db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-      console.log(row.id + ': ' + row.info)
-    })
   })
 })
